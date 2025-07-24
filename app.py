b434776d065af8b7cbafd10132214cdae489f8e7
MatteoVaccaITS <matteo.vacca@its-ictpiemonte.it>
2025-07-24 16:52:54 +0200

    add healt status

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import the required libraries from Flask and flask_cors
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
+import socket
 
 # Create a Flask app
 app = Flask(__name__)
@@ -12,6 +14,18 @@ CORS(app)
 def hello():
     # Return a JSON response with a welcome message
     return jsonify(message="CIAO DA K8S!!!!!!!!e ehheheheee")
+
+@app.route('/health')
+def health_check():
+    # Return a JSON response indicating the service is healthy
+    return jsonify(status="healthy")        
+
+@app.route('/hostname')
+def get_hostname():
+    msg = os.getenv('MESSAGE', 'Hello from k8s!')
+    hostname = socket.gethostname()
+    # Return a JSON response with the hostname and message
+    return jsonify(hostname=hostname, message=msg)
 
 # Start the application only if the file is run directly
 if __name__ == '__main__':
